72cbf19d464af6ace35bcaefa004f2b99c87c4a3
davidbatisson <82897684+davidbatisson@users.noreply.github.com>
2021-06-17 17:01:04 +0200

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -9,6 +9,7 @@ public class Greeter {
   }
 
   public final String greet(final String someone) {
-    return String.format("Hello babacar, %s!", someone);
+    private String message ="Hello babacar, %s!";
+    return String.format(message, someone);
   }
 }
