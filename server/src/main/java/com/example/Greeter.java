2f9b6d9d0b989eac9678dc887df2c8ac5da011be
davidbatisson <82897684+davidbatisson@users.noreply.github.com>
2021-06-18 11:25:26 +0200

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,15 +1,15 @@
 package com.example;
 
-
+// class Greeter
 public class Greeter {
 
-  
   public Greeter() {
 
   }
 
+  // méthode greet
   public final String greet(final String someone) {
-    String message = "Hello " + someone ;
+    String message = "Hello " + someone;
     return message; 
   }
 }
